703d343dec937bef4e982508bba8b83eb68d1aca
Joao Victor <falejoaovictor@hotmail.com>
2021-08-07 21:25:46 -0300

    added influxDB feature

--- orchestrator.py
+++ orchestrator.py
@@ -1,34 +1,51 @@
 from paho.mqtt import client as mqtt_client
 import random
+from influxdb import InfluxDBClient
 
-broker = "localhost"
-port = 1883
-topic = "selfbalancing"
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
+broker_mqtt = "localhost"
+port_mqtt = 1883
+topic_mqtt = "selfbalancing"
+client_id_mqtt = f'python-mqtt-{random.randint(0, 1000)}'
+
+server_influx = "localhost"
+port_influx = 8086
+database_influx = "selfbalancing"
+
+client_influx = InfluxDBClient(host=server_influx, port=port_influx, database=database_influx)
 
 def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(client_mqtt, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
             print("Connection failed!")
-    # set connecting client id
-    client = mqtt_client.Client(client_id)
-    #client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+    client_mqtt = mqtt_client.Client(client_id_mqtt)
+    client_mqtt.on_connect = on_connect
+    client_mqtt.connect(broker_mqtt, port_mqtt)
+    return client_mqtt
 
-def subscribe(client: mqtt_client):
-    def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-    client.subscribe(topic)
-    client.on_message = on_message
+def subscribe(client_mqtt: mqtt_client):
+    def on_message(client_mqtt, userdata, msg):
+        value = str(msg.payload, 'utf-8')
+        json_body = [
+            {
+            "measurement": "selfbalancing",
+            "tags": {},
+            "fields": {
+                "value": value
+                }
+            }
+        ]
+        client_influx.write_points(json_body)
+        print(f"{msg.topic} {value}")
+    client_mqtt.subscribe(topic_mqtt)
+    client_mqtt.on_message = on_message
 
 def run():
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+    client_mqtt = connect_mqtt()
+    subscribe(client_mqtt)
+    client_mqtt.loop_forever()
 
 if __name__ == "__main__":
+    client_influx.create_database(database_influx)
     run()
